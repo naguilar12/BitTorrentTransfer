553e14a2f988e2cf01c461b4c14426ae9d177610
naguilar12 <n.aguilar@uniandes.edu.co>
2018-03-24 20:13:43 -0500

    Sample files

--- src/main/java/DownloadClient/BitTorrentClient.java
+++ src/main/java/DownloadClient/BitTorrentClient.java
@@ -22,45 +22,45 @@ public class BitTorrentClient {
 		try {			
 			// First, instantiate the Client object.
 			Client client = new Client(
-			// This is the interface the client will listen on (you might need something
-			// else than localhost here).
-			InetAddress.getLocalHost(),
+					// This is the interface the client will listen on (you might need something
+					// else than localhost here).
+					InetAddress.getLocalHost(),
 
-			// Load the torrent from the torrent file and use the given
-			// output directory. Partials downloads are automatically recovered.
-			SharedTorrent.fromFile(
-			new File("./clientDownload/Black Label XXL.torrent"),
-			new File("./clientDownloadFiles")));
+					// Load the torrent from the torrent file and use the given
+					// output directory. Partials downloads are automatically recovered.
+					SharedTorrent.fromFile(
+							new File("./clientDownload/Black Label XXL.torrent"),
+							new File("./clientDownloadFiles")));
 
 			// You can optionally set download/upload rate limits
 			// in kB/second. Setting a limit to 0.0 disables rate
 			// limits.
-//			client.setMaxDownloadRate(5000000.0);
-//			client.setMaxUploadRate(500000.0);
+			//			client.setMaxDownloadRate(5000000.0);
+			//			client.setMaxUploadRate(500000.0);
 
 			// At this point, can you either call download() to download the torrent and
 			// stop immediately after...
 			client.download();
 			long a = System.currentTimeMillis();
-			
-			client.addObserver(new Observer() {
-				  public void update(Observable observable, Object data) {
-				    Client client = (Client) observable;
-				    float progress = client.getTorrent().getCompletion();
-				    System.out.println(progress + "%");
-				    // Do something with progress.
-				  }
-				});
-			
-//			URL whatismyip = new URL("http://checkip.amazonaws.com");
-//			BufferedReader in = new BufferedReader(new InputStreamReader(
-//			                whatismyip.openStream()));
-//
-//			String ip = in.readLine(); //you get the IP as a String
-//			System.out.println(ip);
-//			System.out.println(InetAddress.getByName(ip));
 
-			
+			client.addObserver(new Observer() {
+				public void update(Observable observable, Object data) {
+					Client client = (Client) observable;
+					float progress = client.getTorrent().getCompletion();
+					System.out.println(progress + "%");
+					// Do something with progress.
+				}
+			});
+
+			//			URL whatismyip = new URL("http://checkip.amazonaws.com");
+			//			BufferedReader in = new BufferedReader(new InputStreamReader(
+			//			                whatismyip.openStream()));
+			//
+			//			String ip = in.readLine(); //you get the IP as a String
+			//			System.out.println(ip);
+			//			System.out.println(InetAddress.getByName(ip));
+
+
 			//new InetSocketAddress("201.244.249.81", 0);
 
 			// Or call client.share(...) with a seed time in seconds:
@@ -71,8 +71,10 @@ public class BitTorrentClient {
 			// To wait for this process to finish, call:
 			//client.waitForCompletion();
 			System.out.println(System.currentTimeMillis()-a);
-			
+
 			// At any time you can call client.stop() to interrupt the download.
+			while(true)
+				client.getPeers();
 		}
 		catch(Exception e) {
 			e.printStackTrace();
